50c8b4ed67c04ae09b113ae759af1976e2218cff
nsungi <nsungi180@gmail.com>
2024-03-25 12:28:35 -0700

    Your commit message here

--- trade/trade/settings.py
+++ trade/trade/settings.py
@@ -131,10 +131,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
 STATIC_URL = "static/"
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 #mail
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
